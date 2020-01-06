bba4233d609406382eb51f8a49a20038bebe3d77
Paolo Innocenti <innocentipaolo1@gmail.com>
2020-01-06 10:18:00 +0100

    versione da modificare

--- blobDetectionMultiprocessing/modules/keypoints.py
+++ blobDetectionMultiprocessing/modules/keypoints.py
@@ -1,5 +1,4 @@
 import math
-
 import cv2
 import numpy as np
 import numpy.linalg as linalg
@@ -7,12 +6,12 @@ import numpy.linalg as linalg
 
 def filterCandidates(candidates, dogOctave):
     keypoints = []
-    r_th=10
-    t_c=0.03
+    r_th = 10
+    t_c = 0.03
     R_th = (r_th+1)**2 / r_th
 
     for candidate in candidates:
-        y, x, s = candidate[1], candidate[0], candidate[2]
+        x, y, s = candidate[0], candidate[1], candidate[2]
         dx = (dogOctave[y,x+1,s]-dogOctave[y,x-1,s])/2.
         dy = (dogOctave[y+1,x,s]-dogOctave[y-1,x,s])/2.
         ds = (dogOctave[y,x,s+1]-dogOctave[y,x,s-1])/2.
@@ -31,16 +30,19 @@ def filterCandidates(candidates, dogOctave):
             [dxs, dys, dss]])
 
         offset = -linalg.inv(HD).dot(J)
-        contrast = dogOctave[y,x,s] + .5*J.dot(offset)
-        if abs(contrast) < t_c: continue
+        contrast = dogOctave[y, x, s] + .5*J.dot(offset)
+        if abs(contrast) < t_c:
+            continue
 
         w, v = linalg.eig(HD)
         r = w[1]/w[0]
         R = (r+1)**2 / r
-        if R > R_th: continue
+        if R > R_th:
+            continue
 
         kp = np.array([x, y, s]) + offset
-        if kp[1] >= dogOctave.shape[0] or kp[0] >= dogOctave.shape[1]: continue
+        if kp[1] >= dogOctave.shape[0] or kp[0] >= dogOctave.shape[1]:
+            continue
 
         keypoints.append(kp)
     return keypoints
